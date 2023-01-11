c95f8a83a87cd1b6be37255f3ad60d5649ecb442
zerosnacks <zerosnacks@protonmail.com>
2023-01-11 15:33:51 +0100

    clean up

--- test/BaseInstaller.t.sol
+++ test/BaseInstaller.t.sol
@@ -19,10 +19,41 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
     // Constants
     // =========
 
-    uint32 internal constant _SINGLE_MODULE_ID = 100;
-    uint16 internal constant _SINGLE_MODULE_TYPE = _MODULE_TYPE_SINGLE_PROXY;
-    uint16 internal constant _SINGLE_MODULE_VERSION_V1 = 1;
-    uint16 internal constant _SINGLE_MODULE_VERSION_V2 = 2;
+    uint32 internal constant _MODULE_SINGLE_ID = 100;
+    uint16 internal constant _MODULE_SINGLE_TYPE = _MODULE_TYPE_SINGLE_PROXY;
+    uint16 internal constant _MODULE_SINGLE_VERSION_V1 = 1;
+    uint16 internal constant _MODULE_SINGLE_VERSION_V2 = 2;
+    uint16 internal constant _MODULE_SINGLE_VERSION_V3 = 3;
+    bool internal constant _MODULE_SINGLE_UPGRADEABLE_V1 = true;
+    bool internal constant _MODULE_SINGLE_UPGRADEABLE_V2 = true;
+    bool internal constant _MODULE_SINGLE_UPGRADEABLE_V3 = false;
+    bool internal constant _MODULE_SINGLE_REMOVEABLE_V1 = true;
+    bool internal constant _MODULE_SINGLE_REMOVEABLE_V2 = true;
+    bool internal constant _MODULE_SINGLE_REMOVEABLE_V3 = false;
+
+    uint32 internal constant _MODULE_MULTI_ID = 101;
+    uint16 internal constant _MODULE_MULTI_TYPE = _MODULE_TYPE_MULTI_PROXY;
+    uint16 internal constant _MODULE_MULTI_VERSION_V1 = 1;
+    uint16 internal constant _MODULE_MULTI_VERSION_V2 = 2;
+    uint16 internal constant _MODULE_MULTI_VERSION_V3 = 3;
+    bool internal constant _MODULE_MULTI_UPGRADEABLE_V1 = true;
+    bool internal constant _MODULE_MULTI_UPGRADEABLE_V2 = true;
+    bool internal constant _MODULE_MULTI_UPGRADEABLE_V3 = false;
+    bool internal constant _MODULE_MULTI_REMOVEABLE_V1 = true;
+    bool internal constant _MODULE_MULTI_REMOVEABLE_V2 = true;
+    bool internal constant _MODULE_MULTI_REMOVEABLE_V3 = false;
+
+    uint32 internal constant _MODULE_INTERNAL_ID = 102;
+    uint16 internal constant _MODULE_INTERNAL_TYPE = _MODULE_TYPE_INTERNAL;
+    uint16 internal constant _MODULE_INTERNAL_VERSION_V1 = 1;
+    uint16 internal constant _MODULE_INTERNAL_VERSION_V2 = 2;
+    uint16 internal constant _MODULE_INTERNAL_VERSION_V3 = 3;
+    bool internal constant _MODULE_INTERNAL_UPGRADEABLE_V1 = true;
+    bool internal constant _MODULE_INTERNAL_UPGRADEABLE_V2 = true;
+    bool internal constant _MODULE_INTERNAL_UPGRADEABLE_V3 = false;
+    bool internal constant _MODULE_INTERNAL_REMOVEABLE_V1 = true;
+    bool internal constant _MODULE_INTERNAL_REMOVEABLE_V2 = true;
+    bool internal constant _MODULE_INTERNAL_REMOVEABLE_V3 = false;
 
     // =======
     // Storage
@@ -30,6 +61,15 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
 
     MockBaseModule public singleModuleV1;
     MockBaseModule public singleModuleV2;
+    MockBaseModule public singleModuleV3;
+
+    MockBaseModule public multiModuleV1;
+    MockBaseModule public multiModuleV2;
+    MockBaseModule public multiModuleV3;
+
+    MockBaseModule public internalModuleV1;
+    MockBaseModule public internalModuleV2;
+    MockBaseModule public internalModuleV3;
 
     // =====
     // Setup
@@ -40,21 +80,91 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
 
         singleModuleV1 = new MockBaseModule(
             IBaseModule.ModuleSettings({
-                moduleId: _SINGLE_MODULE_ID,
-                moduleType: _SINGLE_MODULE_TYPE,
-                moduleVersion: _SINGLE_MODULE_VERSION_V1,
-                moduleUpgradeable: true,
-                moduleRemoveable: true
+                moduleId: _MODULE_SINGLE_ID,
+                moduleType: _MODULE_SINGLE_TYPE,
+                moduleVersion: _MODULE_SINGLE_VERSION_V1,
+                moduleUpgradeable: _MODULE_SINGLE_UPGRADEABLE_V1,
+                moduleRemoveable: _MODULE_SINGLE_REMOVEABLE_V1
             })
         );
 
         singleModuleV2 = new MockBaseModule(
             IBaseModule.ModuleSettings({
-                moduleId: _SINGLE_MODULE_ID,
-                moduleType: _SINGLE_MODULE_TYPE,
-                moduleVersion: _SINGLE_MODULE_VERSION_V2,
-                moduleUpgradeable: true,
-                moduleRemoveable: true
+                moduleId: _MODULE_SINGLE_ID,
+                moduleType: _MODULE_SINGLE_TYPE,
+                moduleVersion: _MODULE_SINGLE_VERSION_V2,
+                moduleUpgradeable: _MODULE_SINGLE_UPGRADEABLE_V2,
+                moduleRemoveable: _MODULE_SINGLE_REMOVEABLE_V2
+            })
+        );
+
+        singleModuleV3 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_SINGLE_ID,
+                moduleType: _MODULE_SINGLE_TYPE,
+                moduleVersion: _MODULE_SINGLE_VERSION_V3,
+                moduleUpgradeable: _MODULE_SINGLE_UPGRADEABLE_V3,
+                moduleRemoveable: _MODULE_SINGLE_REMOVEABLE_V3
+            })
+        );
+
+        multiModuleV1 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_MULTI_ID,
+                moduleType: _MODULE_MULTI_TYPE,
+                moduleVersion: _MODULE_MULTI_VERSION_V1,
+                moduleUpgradeable: _MODULE_MULTI_UPGRADEABLE_V1,
+                moduleRemoveable: _MODULE_MULTI_REMOVEABLE_V1
+            })
+        );
+
+        multiModuleV2 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_MULTI_ID,
+                moduleType: _MODULE_MULTI_TYPE,
+                moduleVersion: _MODULE_MULTI_VERSION_V2,
+                moduleUpgradeable: _MODULE_MULTI_UPGRADEABLE_V2,
+                moduleRemoveable: _MODULE_MULTI_REMOVEABLE_V2
+            })
+        );
+
+        multiModuleV3 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_MULTI_ID,
+                moduleType: _MODULE_MULTI_TYPE,
+                moduleVersion: _MODULE_MULTI_VERSION_V3,
+                moduleUpgradeable: _MODULE_MULTI_UPGRADEABLE_V3,
+                moduleRemoveable: _MODULE_MULTI_REMOVEABLE_V3
+            })
+        );
+
+        internalModuleV1 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_INTERNAL_ID,
+                moduleType: _MODULE_INTERNAL_TYPE,
+                moduleVersion: _MODULE_INTERNAL_VERSION_V1,
+                moduleUpgradeable: _MODULE_INTERNAL_UPGRADEABLE_V1,
+                moduleRemoveable: _MODULE_INTERNAL_REMOVEABLE_V1
+            })
+        );
+
+        internalModuleV2 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_INTERNAL_ID,
+                moduleType: _MODULE_INTERNAL_TYPE,
+                moduleVersion: _MODULE_INTERNAL_VERSION_V2,
+                moduleUpgradeable: _MODULE_INTERNAL_UPGRADEABLE_V2,
+                moduleRemoveable: _MODULE_INTERNAL_REMOVEABLE_V2
+            })
+        );
+
+        internalModuleV3 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_INTERNAL_ID,
+                moduleType: _MODULE_INTERNAL_TYPE,
+                moduleVersion: _MODULE_INTERNAL_VERSION_V3,
+                moduleUpgradeable: _MODULE_INTERNAL_UPGRADEABLE_V3,
+                moduleRemoveable: _MODULE_INTERNAL_REMOVEABLE_V3
             })
         );
     }
@@ -62,6 +172,89 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
     // =====
     // Tests
     // =====
+
+    function testModuleSettings() external {
+        _testModuleConfiguration(
+            singleModuleV1,
+            _MODULE_SINGLE_ID,
+            _MODULE_SINGLE_TYPE,
+            _MODULE_SINGLE_VERSION_V1,
+            _MODULE_SINGLE_UPGRADEABLE_V1,
+            _MODULE_SINGLE_REMOVEABLE_V1
+        );
+
+        _testModuleConfiguration(
+            singleModuleV2,
+            _MODULE_SINGLE_ID,
+            _MODULE_SINGLE_TYPE,
+            _MODULE_SINGLE_VERSION_V2,
+            _MODULE_SINGLE_UPGRADEABLE_V2,
+            _MODULE_SINGLE_REMOVEABLE_V2
+        );
+
+        _testModuleConfiguration(
+            singleModuleV3,
+            _MODULE_SINGLE_ID,
+            _MODULE_SINGLE_TYPE,
+            _MODULE_SINGLE_VERSION_V3,
+            _MODULE_SINGLE_UPGRADEABLE_V3,
+            _MODULE_SINGLE_REMOVEABLE_V3
+        );
+
+        _testModuleConfiguration(
+            multiModuleV1,
+            _MODULE_MULTI_ID,
+            _MODULE_MULTI_TYPE,
+            _MODULE_MULTI_VERSION_V1,
+            _MODULE_MULTI_UPGRADEABLE_V1,
+            _MODULE_MULTI_REMOVEABLE_V1
+        );
+
+        _testModuleConfiguration(
+            multiModuleV2,
+            _MODULE_MULTI_ID,
+            _MODULE_MULTI_TYPE,
+            _MODULE_MULTI_VERSION_V2,
+            _MODULE_MULTI_UPGRADEABLE_V2,
+            _MODULE_MULTI_REMOVEABLE_V2
+        );
+
+        _testModuleConfiguration(
+            multiModuleV3,
+            _MODULE_MULTI_ID,
+            _MODULE_MULTI_TYPE,
+            _MODULE_MULTI_VERSION_V3,
+            _MODULE_MULTI_UPGRADEABLE_V3,
+            _MODULE_MULTI_REMOVEABLE_V3
+        );
+
+        _testModuleConfiguration(
+            internalModuleV1,
+            _MODULE_INTERNAL_ID,
+            _MODULE_INTERNAL_TYPE,
+            _MODULE_INTERNAL_VERSION_V1,
+            _MODULE_INTERNAL_UPGRADEABLE_V1,
+            _MODULE_INTERNAL_REMOVEABLE_V1
+        );
+
+        _testModuleConfiguration(
+            internalModuleV2,
+            _MODULE_INTERNAL_ID,
+            _MODULE_INTERNAL_TYPE,
+            _MODULE_INTERNAL_VERSION_V2,
+            _MODULE_INTERNAL_UPGRADEABLE_V2,
+            _MODULE_INTERNAL_REMOVEABLE_V2
+        );
+
+        _testModuleConfiguration(
+            internalModuleV3,
+            _MODULE_INTERNAL_ID,
+            _MODULE_INTERNAL_TYPE,
+            _MODULE_INTERNAL_VERSION_V3,
+            _MODULE_INTERNAL_UPGRADEABLE_V3,
+            _MODULE_INTERNAL_REMOVEABLE_V3
+        );
+    }
 
     // ===============
     // Ownership tests
@@ -150,7 +343,7 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
             new MockBaseModule(
                 IBaseModule.ModuleSettings({
                     moduleId: 777,
-                    moduleType: _SINGLE_MODULE_TYPE,
+                    moduleType: _MODULE_SINGLE_TYPE,
                     moduleVersion: 777,
                     moduleUpgradeable: true,
                     moduleRemoveable: true
@@ -186,7 +379,7 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
             new MockBaseModule(
                 IBaseModule.ModuleSettings({
                     moduleId: 777,
-                    moduleType: _SINGLE_MODULE_TYPE,
+                    moduleType: _MODULE_SINGLE_TYPE,
                     moduleVersion: 777,
                     moduleUpgradeable: true,
                     moduleRemoveable: true
@@ -226,21 +419,21 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
 
         vm.expectEmit(true, true, false, false);
         emit ModuleAdded(
-            _SINGLE_MODULE_ID,
+            _MODULE_SINGLE_ID,
             address(singleModuleV1),
-            _SINGLE_MODULE_VERSION_V1
+            _MODULE_SINGLE_VERSION_V1
         );
         installerProxy.addModules(moduleAddresses);
 
         address singleModuleV1Implementation = dispatcher
-            .moduleIdToImplementation(_SINGLE_MODULE_ID);
-        address moduleProxy = dispatcher.moduleIdToProxy(_SINGLE_MODULE_ID);
+            .moduleIdToImplementation(_MODULE_SINGLE_ID);
+        address moduleProxy = dispatcher.moduleIdToProxy(_MODULE_SINGLE_ID);
 
         assertEq(singleModuleV1Implementation, address(singleModuleV1));
         assertTrue(moduleProxy != address(0));
         assertTrue(
             dispatcher.proxyAddressToTrustRelation(moduleProxy).moduleId ==
-                _SINGLE_MODULE_ID
+                _MODULE_SINGLE_ID
         );
         assertTrue(
             dispatcher
@@ -253,31 +446,31 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
         testAddModulesSingleProxy();
 
         address singleModuleV1Implementation = dispatcher
-            .moduleIdToImplementation(_SINGLE_MODULE_ID);
-        address moduleProxy = dispatcher.moduleIdToProxy(_SINGLE_MODULE_ID);
+            .moduleIdToImplementation(_MODULE_SINGLE_ID);
+        address moduleProxy = dispatcher.moduleIdToProxy(_MODULE_SINGLE_ID);
 
         address[] memory moduleAddresses = new address[](1);
         moduleAddresses[0] = address(singleModuleV2);
 
         vm.expectEmit(true, true, false, false);
         emit ModuleUpgraded(
-            _SINGLE_MODULE_ID,
+            _MODULE_SINGLE_ID,
             address(singleModuleV2),
-            _SINGLE_MODULE_VERSION_V1
+            _MODULE_SINGLE_VERSION_V1
         );
         installerProxy.upgradeModules(moduleAddresses);
 
         address singleModuleV2Implementation = dispatcher
-            .moduleIdToImplementation(_SINGLE_MODULE_ID);
+            .moduleIdToImplementation(_MODULE_SINGLE_ID);
 
-        assertEq(moduleProxy, dispatcher.moduleIdToProxy(_SINGLE_MODULE_ID));
+        assertEq(moduleProxy, dispatcher.moduleIdToProxy(_MODULE_SINGLE_ID));
         assertTrue(
             singleModuleV1Implementation != singleModuleV2Implementation
         );
         assertEq(singleModuleV2Implementation, address(singleModuleV2));
         assertTrue(
             dispatcher.proxyAddressToTrustRelation(moduleProxy).moduleId ==
-                _SINGLE_MODULE_ID
+                _MODULE_SINGLE_ID
         );
         assertTrue(
             dispatcher
@@ -289,22 +482,22 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
     function testRemoveModulesSingleProxy() external {
         testAddModulesSingleProxy();
 
-        address moduleProxy = dispatcher.moduleIdToProxy(_SINGLE_MODULE_ID);
+        address moduleProxy = dispatcher.moduleIdToProxy(_MODULE_SINGLE_ID);
 
         address[] memory moduleAddresses = new address[](1);
         moduleAddresses[0] = address(singleModuleV1);
 
         vm.expectEmit(true, true, false, false);
         emit ModuleRemoved(
-            _SINGLE_MODULE_ID,
+            _MODULE_SINGLE_ID,
             address(singleModuleV1),
-            _SINGLE_MODULE_VERSION_V1
+            _MODULE_SINGLE_VERSION_V1
         );
         installerProxy.removeModules(moduleAddresses);
 
-        assertEq(dispatcher.moduleIdToProxy(_SINGLE_MODULE_ID), address(0));
+        assertEq(dispatcher.moduleIdToProxy(_MODULE_SINGLE_ID), address(0));
         assertEq(
-            dispatcher.moduleIdToImplementation(_SINGLE_MODULE_ID),
+            dispatcher.moduleIdToImplementation(_MODULE_SINGLE_ID),
             address(0)
         );
         assertEq(
@@ -319,6 +512,26 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
         );
     }
 
+    function testRevertUpgradeModulesNonUpgradeableSingleProxy() external {
+        singleModuleV1 = new MockBaseModule(
+            IBaseModule.ModuleSettings({
+                moduleId: _MODULE_SINGLE_ID,
+                moduleType: _MODULE_SINGLE_TYPE,
+                moduleVersion: _MODULE_SINGLE_VERSION_V1,
+                moduleUpgradeable: true,
+                moduleRemoveable: true
+            })
+        );
+
+        // TODO: add tests
+    }
+
+    function testRevertUpgradeModulesNonRemoveableSingleProxy() external {
+        testAddModulesSingleProxy();
+
+        // TODO: add tests
+    }
+
     // ========================
     // Multi-proxy module tests
     // ========================
@@ -329,7 +542,7 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
             new MockBaseModule(
                 IBaseModule.ModuleSettings({
                     moduleId: 2,
-                    moduleType: _MODULE_TYPE_MULTI_PROXY,
+                    moduleType: _MODULE_MULTI_TYPE,
                     moduleVersion: 1,
                     moduleUpgradeable: true,
                     moduleRemoveable: true
@@ -340,7 +553,7 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
             new MockBaseModule(
                 IBaseModule.ModuleSettings({
                     moduleId: 3,
-                    moduleType: _MODULE_TYPE_MULTI_PROXY,
+                    moduleType: _MODULE_MULTI_TYPE,
                     moduleVersion: 1,
                     moduleUpgradeable: true,
                     moduleRemoveable: true
@@ -356,35 +569,27 @@ contract BaseInstallerTest is TBaseInstaller, BaseFixture {
         );
     }
 
+    function testUpgradeModulesMultiProxy() external {
+        // TODO: add tests
+    }
+
+    function testRemoveModulesMultiProxy() external {
+        // TODO: add tests
+    }
+
     // =====================
     // Internal module tests
     // =====================
 
-    // function testAddModulesInternal() external {
-    //     address[] memory moduleAddresses = new address[](1);
-    //     moduleAddresses[0] = address(
-    //         new MockBaseModule(
-    //             IBaseModule.ModuleSettings({
-    //                 moduleId: 2,
-    //                 moduleType: _MODULE_TYPE_INTERNAL,
-    //                 moduleVersion: _MODULE_VERSION_DEFAULT,
-    //                 moduleUpgradeable: true,
-    //                 moduleRemoveable: true
-    //             })
-    //         )
-    //     );
-    //     installerProxy.addModules(moduleAddresses);
+    function testAddModulesInternal() external {
+        // TODO: add tests
+    }
 
-    //     assertEq(singleModuleV1Implementation, address(singleModuleV1));
-    //     assertTrue(moduleProxy != address(0));
-    //     assertTrue(
-    //         dispatcher.proxyAddressToTrustRelation(moduleProxy).moduleId ==
-    //             _SINGLE_MODULE_ID
-    //     );
-    //     assertTrue(
-    //         dispatcher
-    //             .proxyAddressToTrustRelation(moduleProxy)
-    //             .moduleImplementation == singleModuleV1Implementation
-    //     );
-    // }
+    function testUpgradeModulesInternal() external {
+        // TODO: add tests
+    }
+
+    function testRemoveModulesInternal() external {
+        // TODO: add tests
+    }
 }
