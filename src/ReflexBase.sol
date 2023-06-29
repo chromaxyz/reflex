bbcd7335664e97af37b665a2d1b75e5cf358b159
zerosnacks <zerosnacks@protonmail.com>
2023-06-29 18:42:48 +0200

    notes

--- src/ReflexBase.sol
+++ src/ReflexBase.sol
@@ -117,6 +117,9 @@ abstract contract ReflexBase is IReflexBase, ReflexState {
      * @return bytes Call result.
      */
     function _callInternalModule(uint32 moduleId_, bytes memory input_) internal virtual returns (bytes memory) {
+        // TODO: check existence of module and zero-check
+        // TODO: Check if module is internal.
+
         (bool success, bytes memory result) = _REFLEX_STORAGE().modules[moduleId_].delegatecall(input_);
 
         if (!success) _revertBytes(result);
